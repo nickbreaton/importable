4c0cd0ca60ac6ff987d27517b749135882a41650
Nick Breaton <nick@nickbreaton.com>
2024-08-08 23:47:48 -0400

    also listen for mouse enter events

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -89,7 +89,8 @@ function useRevalidateOnWindowFocus() {
   const revalidator = useRevalidator();
   useEffect(() => {
     const controller = new AbortController();
-    window.addEventListener("focus", () => revalidator.revalidate(), { signal: controller.signal });
+    window.addEventListener("focus", revalidator.revalidate, { signal: controller.signal });
+    document.addEventListener("mouseenter", revalidator.revalidate, { signal: controller.signal });
     return () => controller.abort();
   }, [revalidator]);
 }

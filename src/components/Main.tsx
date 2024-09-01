d25cdd045e80ed67820e5254614fd064f3433a22
Nick Breaton <nick@nickbreaton.com>
2024-09-01 12:49:11 -0400

    offshoot with effect

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,15 +1,7 @@
-import { useEffect } from "react";
+import { useState, useSyncExternalStore } from "react";
 import localForage from "localforage";
-import {
-  assign,
-  fromPromise,
-  setup,
-  stopChild,
-  type ActorRef,
-  type ActorRefFrom,
-  type AnyActorRef,
-} from "xstate";
-import { useActor } from "@xstate/react";
+import { assign, fromPromise, setup, stopChild, type ActorRef, type ActorRefFrom, type AnyActorRef } from "xstate";
+import { Effect, Layer, ManagedRuntime, Ref, Stream, SubscriptionRef } from "effect";
 
 const CRAWL_DISALLOW = [
   /^\./, // starts with dot
@@ -41,9 +33,7 @@ async function crawlDirectoryHandle({
     const path = [precedingPaths, handle.name].filter(Boolean).join("/");
 
     // maintain stable reference
-    handle = (await previousFiles[path]?.isSameEntry(handle))
-      ? previousFiles[path]
-      : handle;
+    handle = (await previousFiles[path]?.isSameEntry(handle)) ? previousFiles[path] : handle;
 
     if (handle.kind === "file") {
       tree = { ...tree, [path]: handle };
@@ -60,20 +50,64 @@ async function crawlDirectoryHandle({
   return tree;
 }
 
-const treeCrawler = fromPromise<
-  void,
-  { directoryHandle: FileSystemDirectoryHandle; parent: AnyActorRef }
->(async ({ signal, input: { directoryHandle, parent } }) => {
-  const result = await crawlDirectoryHandle({
-    signal,
-    directoryHandle,
-    previousFiles: {},
-  });
+const treeCrawler = fromPromise<void, { directoryHandle: FileSystemDirectoryHandle; parent: AnyActorRef }>(
+  async ({ signal, input: { directoryHandle, parent } }) => {
+    const result = await crawlDirectoryHandle({
+      signal,
+      directoryHandle,
+      previousFiles: {},
+    });
 
-  if (!signal.aborted) {
-    parent.send({ type: "REFRESH_COMPLETE", files: result });
+    if (!signal.aborted) {
+      parent.send({ type: "REFRESH_COMPLETE", files: result });
+    }
+  },
+);
+
+type State =
+  | { type: "ejected" }
+  | { type: "selecting" }
+  | {
+      type: "selected";
+      directory: FileSystemDirectoryHandle;
+      files?: FileTree;
+    };
+
+class CrawlerActor {
+  constructor() {}
+}
+
+class MainActor {
+  get: Effect.Effect<State>;
+
+  showDirectoryPicker: () => Effect.Effect<void>;
+  eject: () => Effect.Effect<void>;
+
+  changes: Stream.Stream<State>;
+
+  private constructor(private state: SubscriptionRef.SubscriptionRef<State>) {
+    this.get = Ref.get(state);
+    this.changes = this.state.changes;
+
+    this.showDirectoryPicker = () =>
+      Ref.set(state, { type: "selecting" }).pipe(
+        Effect.andThen(() => {
+          return Effect.tryPromise({
+            try: () => window.showDirectoryPicker(),
+            catch: Effect.fail,
+          });
+        }),
+        Effect.andThen((directory) => Ref.set(state, { type: "selected", directory })),
+        Effect.catchAll(() => Ref.set(state, { type: "ejected" })),
+      );
+
+    this.eject = () => Ref.set(state, { type: "ejected" });
   }
-});
+
+  static make(initial: State) {
+    return SubscriptionRef.make(initial).pipe(Effect.map((ref) => new MainActor(ref)));
+  }
+}
 
 const program = setup({
   types: {
@@ -97,8 +131,7 @@ const program = setup({
       // This ensures we can properly store a directory handle.
       await localForage.setDriver(localForage.INDEXEDDB);
 
-      const directory =
-        await localForage.getItem<FileSystemDirectoryHandle>("directory");
+      const directory = await localForage.getItem<FileSystemDirectoryHandle>("directory");
       // TODO: see if we can make a good UX with ultra qukck loading
       await new Promise((res) => setTimeout(res, 500));
       return directory;
@@ -179,40 +212,47 @@ const program = setup({
 
 type FileTree = Record<string, FileSystemFileHandle>;
 
-export function Main() {
-  const [actor, send] = useActor(program);
+// export function useSynchronizedState<T>(ref: Effect.Effect<SubscriptionRef.SubscriptionRef<T>>) {
+//   const [subscriptionRef] = useState(Effect.runSync(ref));
 
-  useEffect(() => {
-    const controller = new AbortController();
-    const refresh = () => send({ type: "REFRESH" });
-    refresh();
-    window.addEventListener("focus", refresh, {
-      signal: controller.signal,
-    });
-    document.addEventListener("mouseenter", refresh, {
-      signal: controller.signal,
-    });
-    return () => controller.abort();
-  }, []);
+//   const value = useSyncExternalStore(
+//     (callback) => Stream.runForEach(subscriptionRef.changes, () => Effect.sync(callback)).pipe(Effect.runCallback),
+//     () => Effect.runSync(SubscriptionRef.get(subscriptionRef)),
+//   );
+
+//   return [value, subscriptionRef] as const;
+// }
+
+export function useMainActor() {
+  const [runtime] = useState(() => ManagedRuntime.make(Layer.empty));
+  const [actor] = useState(() => runtime.runSync(MainActor.make({ type: "ejected" })));
+
+  const value = useSyncExternalStore(
+    (callback) => Stream.runForEach(actor.changes, () => Effect.sync(callback)).pipe(Effect.runCallback),
+    () => Effect.runSync(actor.get),
+  );
+
+  return [value, actor] as const;
+}
+
+export function Main() {
+  // const [actor, send] = useActor(program);
+  // const [value, subscriptionRef] = useSynchronizedState(refEf);
+  const [state, actor] = useMainActor();
 
   return (
     <div className="flex gap-4 flex-col">
       <div className="flex gap-4">
-        <button onClick={() => send({ type: "SHOW_DIRECTORY_PICKER" })}>
-          open
-        </button>
-        {actor.can({ type: "EJECT" }) && (
-          <button onClick={() => send({ type: "EJECT" })}>reset</button>
-        )}
+        <button onClick={() => Effect.runPromise(actor.showDirectoryPicker())}>open</button>
+        <button onClick={() => Effect.runPromise(actor.eject())}>reset</button>
       </div>
-      <p className="block">
-        {actor.value} : {actor.context.directory?.name}
-      </p>
-      <ul>
-        {Object.entries(actor.context.files ?? {})?.map(([path, file]) => {
-          return <li key={path}>{path}</li>;
-        })}
-      </ul>
+      <p className="block">{state.type}</p>
     </div>
   );
 }
+
+// <ul>
+//   {Object.entries(actor.context.files ?? {})?.map(([path, file]) => {
+//     return <li key={path}>{path}</li>;
+//   })}
+// </ul>
